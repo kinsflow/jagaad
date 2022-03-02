a952faac5e86fc333455b0390ab32e808d6d097e
kingsley <kingsley.davidakindele@gmail.com>
2022-03-02 21:03:48 +0100

    Complete Step 2 task by designing the API in the STEP2SOLUTION.MD file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/cities', [CityController::class, 'index']);
+Route::get('/cities/{city_id}', [CityController::class, 'show']);
